9f82b1989f92de30fa2a457011be07805bd64c3e
Samdepie <55203554+Samdepie@users.noreply.github.com>
2019-09-25 04:13:44 +0800

    Add files via upload

---
+++ Source.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	cout << "University :\tUniversity Teknikal Malaysia, Melaka";
+	cout << "\nFaculty :\tFTMK";
+	cout << "\nProgramme :\tBITP 1113";
+	cout << "\n\nMy Name is M. Samudra Nusantara. I am from Indonesia";
+	cout << "\n\n\nxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx";
+	cout << "\nxxxxxxxxx\tUTEM\txxxxxxxxxxx";
+	cout << "\nxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx";
+	cout << "\n10";
+	cout << "\n9";
+	cout << "\n8";
+	cout << "\n7\t\tX";
+	cout << "\n6 X";
+	cout << "\n5\t\t\tX";
+	cout << "\n4   X";
+	cout << "\n3\t\t   X";
+	cout << "\n2     X";
+	cout << "\n1 X";
+	cout << "\n0 1 2 3 4 5 6 7 8 9 10";
+	cout << "\n\n\nPress any key to continue . . .";
+
+	return 0;
+}
